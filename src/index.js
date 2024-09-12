9c379cc90a64d1cc68bc4c89b1574cdef45d786f
JALLMAR ANDRES SAMUELS TORRES <jallmar.samuels@ucr.ac.cr>
2024-09-12 10:55:17 -0600

    Creacion de Lista

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log("HOLA MUNDO");
+
+class Main {
+
+    listaPersonas = ["Oscar", "Lara", 2];
+
+    listar() {
+        this.listaPersonas.map((persona) => {
+            console.log(persona);
+        })
+    }
+
+}
+
+const Jallmar = new Main();
+
+Jallmar.listar();
+
+
